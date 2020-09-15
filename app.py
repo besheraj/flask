5baf702d94f582fc066ba5dda88f7dc3ea87927c
Behser Aj <besher.aj@outlook.com>
2020-09-15 14:34:58 +0800

    ini commit

--- app.py
+++ app.py
@@ -43,3 +43,7 @@ def contact():
 @app.route('/careers')
 def careers():
     return render_template("careers.html",page_title="Careers")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
