5d1b5adbfc0c2e69140d3caa3f85772e450e822b
Behser Aj <besher.aj@outlook.com>
2020-09-15 14:55:54 +0800

    ini committ

--- app.py
+++ app.py
@@ -47,5 +47,4 @@ def careers():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    print("Application should start on port "+ str(port))
     app.run(host='0.0.0.0', port=port)
