fcc4c867bf0f46976957c6b327f049d13982ae57
Behser Aj <besher.aj@outlook.com>
2020-09-15 14:49:32 +0800

    Remove static port & host

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def careers():
     return render_template("careers.html",page_title="Careers")
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP", "0.0.0.0"))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
